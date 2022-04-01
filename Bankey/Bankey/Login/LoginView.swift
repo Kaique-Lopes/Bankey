f9a9acad36d89ddc4fdd06c43329f8da9b1ece0c
Kaique <kaique.lopes3271@gmail.com>
2022-03-31 22:45:24 -0300

    init Class

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -9,7 +9,15 @@ import Foundation
 import UIKit
 
 class LoginView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension LoginView {
